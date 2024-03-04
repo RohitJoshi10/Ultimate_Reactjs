0f820b0b71772e3848eb540ba9c5653dcfadcd12
Rohit <rohit11joshi10@gmail.com>
2024-03-04 20:44:44 +0530

    useEffect Hook done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./index.css";
 import UseStateHook from "./Hooks/UseStateHook/UseStateHook";
 import Props from "./Props/Props";
+import UseEffectHook from "./Hooks/UseEffectHook/UseEffectHook";
 
 function App() {
   return (
     <div className="App">
       {/* <UseStateHook /> */}
-      <Props />
+      {/* <Props /> */}
+      <UseEffectHook />
     </div>
   );
 }
--- myapp/src/Hooks/UseEffectHook/UseEffectHook.jsx
+++ myapp/src/Hooks/UseEffectHook/UseEffectHook.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function UseEffectHook() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("Radha");
+
+  useEffect(() => {
+    console.log("Component Mounted");
+  }, [data]);
+
+  function updateCount() {
+    setCount(count + 1);
+  }
+
+  function updateData() {
+    setData("Krishna");
+  }
+
+  return (
+    <>
+      <h1>Button Clicked {count} times</h1>
+      <h1>Bolo Bolo Birajwasi... {data} bolo</h1>
+      <button onClick={updateCount}>Click</button>
+      <button onClick={updateData}>Update Data</button>
+    </>
+  );
 }
 
 export default UseEffectHook;
