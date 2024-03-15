f9efcfad11cf10310f5c4b2c200a9719cf2daf86
Rohit <rohit11joshi10@gmail.com>
2024-03-15 21:07:50 +0530

    miniContext Learn Context API

---
+++ minicontext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext();
+export default UserContext;
+
+// Jaise he aap context bana doge toh wo hume ek major cheez deta hai jisko hum bolte hai provider.
+// So, har ek context ek provider deta hai.
